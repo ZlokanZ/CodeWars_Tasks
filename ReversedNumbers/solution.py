7022041c769c45630c3a6033ba0be870e049bbb4
ZlokanZ <ZlokanZM@gmail.com>
2024-06-11 20:51:53 +0200

    Tym razem prościzna

---
+++ ReversedNumbers/solution.py
@@ -0,0 +1,5 @@
+def digitize(n):
+    return [int(x) for x in reversed(str(n))]
+
+# I found that my self, I was only need to search how to reverse str in python
+# bcs I couldn't reverse int as it should be, but I am just stupid XD
